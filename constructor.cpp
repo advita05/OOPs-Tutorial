98773eef0393fdde926e7ce82a5e8d97d811a070
Advita Talekar <advita.18060@sakec.ac.in>
2025-11-11 14:10:56 +0530

    constructor

--- constructor.cpp
+++ constructor.cpp
@@ -1,3 +1,11 @@
+//Constructors are special class members which are called by the compiler every time an object of that class is instantiated.
+//Constructors have the same name as the class and may be defined inside or outside the class definition.
+//There are 3 types of constructors:
+
+//1. Default constructors
+//2. Parameterized constructors
+//3. Copy constructors
+
 #include <iostream>
 using namespace std;
 
